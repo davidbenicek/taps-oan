2bbb6599b69d1417977c2b7c6ab9b5394c51ef28
kolox- <colinbutler1995@gmail.com>
2017-03-04 14:58:31 +0000

    Fixed beer.title reference in BeerForm

--- taps_oan/forms.py
+++ taps_oan/forms.py
@@ -46,15 +46,15 @@ class UserProfileForm(forms.ModelForm):
 
 
 class BeerForm(forms.Form):
-    title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the beer.")
+    name = forms.CharField(max_length=128,
+                            help_text="Please enter the name of the beer.")
 
     # views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     # likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     # slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
-        fields = ('title')
+        fields = ('name')
 
 
 class CarrierForm(forms.Form):
