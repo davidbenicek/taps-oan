ec0b7bf9fe46eafd60c8349dac8bab9e98173310
kolox- <colinbutler1995@gmail.com>
2017-03-09 17:54:44 +0000

    Added some generic tests to project

--- project/settings.py
+++ project/settings.py
@@ -64,7 +64,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,6 +71,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
--- taps_oan/tests.py
+++ taps_oan/tests.py
@@ -1,13 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from django.contrib.auth.models import AnonymousUser, User
+from taps_oan import views, urls
+from .views import index
 
 import unittest
 
-class PageAccessTests(TestCase):
+class PageAccessTestCase(TestCase):
 
-    def page_loads(self):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test2', email='test@test2.com', password='passpass')
+        self.factory = RequestFactory()
 
-        request = 'placeholder'
+    def test_index(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
 
-        response = index(request)
+    def test_about(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
 
+    def test_user(self):
+        response = self.client.post("/taps_oan/register/", {
+            "username": "testy",
+            "email": "email@email.com", 
+            "password": "password", 
+            "password-confirm": "password" })
+        user = User.objects.get(username="testy")
+        self.assertEqual(user.email, "email@email.com")
         self.assertEqual(response.status_code, 200)
